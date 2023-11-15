76c7515fdeb367edbc208bbc9f26ef96c6ae0879
Thiernoh <thiernohgradiagram@gmail.com>
2023-11-14 19:33:54 -0500

    mercedes end point wip

--- mercedes/mercedesController.ts
+++ mercedes/mercedesController.ts
@@ -1,30 +1,60 @@
 import express from 'express';
-import {Request, Response, Router} from 'express-serve-static-core';
-import { getMercedes } from './mercedesService';
+import {Request, Response, Router, NextFunction} from 'express-serve-static-core';
+import { getAllMercedes, getMercedesByVinNumber, getMercedesByYearAndColor, insertMercedes } from './mercedesService';
 import {expressDotRouterOptions} from '../middlewares/built-in-middleware-config/express.Router.config';
+import { RowDataPacket, FieldPacket, ResultSetHeader } from 'mysql2';
+import { Mercedes } from './mercedes';
 
 export const mercedesRouter: Router  = express.Router(expressDotRouterOptions);
+// https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+// https://github.com/sidorares/node-mysql2/blob/HEAD/documentation/en/TypeScript-Examples.md
 
-mercedesRouter.get('/', async (req: Request, res: Response) => {
-    const mercedes = await getMercedes();
-    res.write(JSON.stringify(mercedes, null, 2));
-    res.end();
+
+mercedesRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    getAllMercedes()
+    .then((result: [RowDataPacket[], FieldPacket[]])=> {
+        const [data] = result as RowDataPacket[];                   // result[0]
+        const tableDescription: FieldPacket[] = result[1];          // result[1]
+        console.log(tableDescription[0]);
+        console.log(data);
+        res.status(200).write(JSON.stringify(data, null, 2));
+        res.end();
+    }).catch((error: any) => next(error));
 });
 
-mercedesRouter.get('/routeParameters/:carId', (req: Request, res: Response) => {
-    res.send(req.params.carId);
+mercedesRouter.get('/:vin_number', (req: Request, res: Response, next: NextFunction) => {
+    getMercedesByVinNumber(req.params.vin_number)
+    .then((result: [RowDataPacket[], FieldPacket[]]) => {
+        const [data] = result as RowDataPacket[];                   // result[0]
+        const tableDescription: FieldPacket[] = result[1];          // result[1]
+        console.log(tableDescription);
+        console.log(data[0]);
+        res.status(200).send(data[0]);
+    }).catch((error: any) => next(error));
 });
 
-mercedesRouter.get('/routeParameters/:year/:month', (req: Request, res: Response) => {
-    res.send(req.params);
+mercedesRouter.get('/:year/:color', (req: Request, res: Response, next: NextFunction) => {
+    getMercedesByYearAndColor(req.params.year, req.params.color)
+    .then((result: [RowDataPacket[], FieldPacket[]]) => {
+        const [data] = result as RowDataPacket[];                   // result[0]
+        const tableDescription: FieldPacket[] = result[1];          // result[1]
+        console.log(tableDescription);
+        console.log(data);
+        res.status(200).send(data);
+    }).catch((error: any) => next(error));
 });
 
-mercedesRouter.get('/queryString', (req: Request, res: Response) => {
+// When to use query param vs query string ??
+mercedesRouter.get('/queryString', (req: Request, res: Response, next: NextFunction) => {
     res.send(req.query);
 });
 
-mercedesRouter.post('/', (req: Request, res: Response) => {
-    res.send('Got a POST request at /cars');
+mercedesRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+    // TODO: input validation with JOI
+   const requestBody: Mercedes = req.body;
+   insertMercedes(requestBody)
+   .then((result: [ResultSetHeader, FieldPacket[]]) => res.status(201).send(result))
+   .catch((error: any) => next(error));
 });
 
 mercedesRouter.put('/', (req: Request, res: Response) => {
--- mercedes/mercedesService.ts
+++ mercedes/mercedesService.ts
@@ -1,52 +1,46 @@
-import { pool } from "../db.connection.config";
-import { Mercedes } from "./mercedes";
+import {pool} from "../db.connection.config";
+import {Mercedes} from "./mercedes";
+import {ResultSetHeader, RowDataPacket} from "mysql2";
 
-
-export async function getMercedes() {
+export async function getAllMercedes() {
     const sql: string = `
         SELECT * 
-        FROM mercedes
-    `
-
-    const result = await pool.query(sql);
-    const rows = result[0];
-    const metaData = result[1];
-    console.log(metaData);
-    return rows;
+        FROM mercedes`;
+    return await pool.query<RowDataPacket[]>(sql);
 }
 
-//getMercedes().then(rows => console.log(rows));
-
-async function getMercedesByVinNumber(vinNumber: string) {
+export async function getMercedesByVinNumber(vinNumber: string) {
     const sql: string = `
         SELECT * 
         FROM mercedes 
-        WHERE vin_number = ?
-    `
-
-    const result = await pool.query(sql, [vinNumber]);
-    const rows = result[0];
-    const metaData = result[1];
-    //console.log(metaData);
-    return rows;
+        WHERE vin_number = ?`;
+    return await pool.query<RowDataPacket[]>(sql, [vinNumber]);
 }
 
-// getMercedesByVinNumber('benz-000-000-040').then(rows => console.log(rows));
+export async function getMercedesByYearAndColor(year: string, color: string) {
+    const sql: string =`
+    SELECT * 
+    FROM mercedes
+    WHERE _year = ? AND color= ?`;
+    return await pool.query<RowDataPacket[]>(sql, [year, color]);
+}
 
-async function insertMercedes(mercedes: Mercedes) {
+
+// INSERT: use ResultSetHeader
+export async function insertMercedes(mercedes: Mercedes) {
     const sql: string = `
         INSERT INTO mercedes
         VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    `
-
-    const result = await pool.query(sql, Object.values(mercedes));
-
-    const rows = result[0];
-    const metaData = result[1];
-    //console.log(result);
-    return result;
+    `;
+    return await pool.query<ResultSetHeader>(sql, Object.values(mercedes));
 }
 
 //const mercedes1 = new Mercedes("benz-000-000-046","green","GLA 250 4MATIC SUV",2023,96778.99,"automatic","gas",4,1111,"fwd",null,4,null);
 //insertMercedes(mercedes1)
 //.then(result => console.log(result));
+
+// Update mercedes: use ResultSetHeader
+
+// Delete mercesdes: ResultSetHeader
+
+// Truncate: use ResultSetHeader
